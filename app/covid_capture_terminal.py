11e05c9ca927db82110df7e2813dc70b68a25e74
Frederik Blais <le1763111@crc-lennox.qc.ca>
2021-03-15 16:01:18 -0400

    end of the day commit

--- app/covid_capture_terminal.py
+++ app/covid_capture_terminal.py
@@ -1,5 +1,8 @@
 # randomly generate one data file per classroom, you must generate 5 classrooms for one day.
 import os
+import os.path
+import datetime
+import time
 
 # Clear the screen.
 os.system('clear')
@@ -12,14 +15,45 @@ print (line)
 print (title)
 print (line)
 
+# Set data directory
+os.chdir("app")
+os.chdir(".data")
+
+# Create 5 txt files for the day
 def create_file():
-    str = "Data file generated"
-    print(str)
+    file_count = 0
 
+    while file_count < 5:
+        cwd = os.getcwd()
+
+        # Create file "classroom(number).txt"
+        file_name = os.path.join(cwd, "classroom"+str(file_count + 1)+".txt")
+
+        txt_file = open(file_name, "w")
+
+        #
+        student_file = open("students.txt","r")
+        for _ in range(1,20+1):
+            date = datetime.date.today()
+
+            rfid_data = student_file.readline()
+            formated_data = str(rfid_data)+str(date)+os.linesep
+            fd = str.join(";", formated_data.splitlines())
+            toFile = fd
+            txt_file.writelines(toFile)
+
+        txt_file.close()
+
+        resp = "Data file " + str(file_count + 1) + " generated"
+        print(resp)
+        file_count += 1
+
+# Delete all the files in .data
 def delete_file():
-    str = "Data file deleted"
-    print(str)
+    resp = "Data file deleted"
+    print(resp)
 
+# Input Loop, break if "x" is selected
 while True:
 
     print()
@@ -37,6 +71,7 @@ while True:
         delete_file()
 
     elif choice == "x":
+        os.system('clear')
         break
     
     else:
